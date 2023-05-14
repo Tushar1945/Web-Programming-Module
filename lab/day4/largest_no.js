12417c3e478378895869af900c1734c1294033c9
Tushar Chaudhari <tusharpc20000@gmail.com>
2023-05-14 17:19:38 +0530

    day1

--- lab/day4/largest_no.js
+++ lab/day4/largest_no.js
@@ -1,4 +1,15 @@
-function large_no(){
+function large_no(p1,p2,p3){
 
+    if((p1>p2) && (p1>p3))
+    {
+        alert( p1+" is greater");
+    }
+    else if((p2>p1) && (p2>p3) )
+    {
+        alert(p2 + " is greater ");
+    }
+    else{
+        alert(p3 + " is greater");
+    }
     
 }
