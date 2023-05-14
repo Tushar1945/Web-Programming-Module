cbf908960976eb35b993f238bfdff4ce722e1301
Tushar Chaudhari <tusharpc20000@gmail.com>
2023-05-14 17:15:14 +0530

    first

---
+++ lab/day3/4.js
@@ -0,0 +1,8 @@
+function sum(){
+let input1 =document.querySelector("#id1").value;
+let input2 =document.querySelector("#id2").value
+let result = parseInt(input1)+parseInt(input2);
+
+
+alert( "input1"   + input1 +   "input2"  + input2 +    "result = " + result );
+} 
